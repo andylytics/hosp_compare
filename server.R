df46681b408a88d11665ed286d9dacc56a90394e
andy <ajr.rosa@gmail.com>
2014-08-13 10:33:04 -0400

    added smoothing option

--- server.R
+++ server.R
@@ -16,7 +16,7 @@ shinyServer(function(input, output) {
     pd <- filter(d, variable %in% c(input$var, benchmarks))
     pd$variable <- factor(pd$variable, levels = c(input$var, benchmarks))
     
-    if (input$cbvar == FALSE){
+    if (input$radio == 1){
       ggplot(pd, aes(EndDate, value, colour = factor(variable))) +
         #stat_smooth(method = "lm", aes(fill = factor(variable))) +
         geom_line() +
@@ -28,7 +28,7 @@ shinyServer(function(input, output) {
         ggtitle(paste0("HCAHPS Percent of Patients\nwho gave a 9 or 10 Rating")) +
         theme(axis.text.x = element_text(angle = 90, size = 14), axis.title.x = element_text(size = 14), axis.title.y = element_text(size = 12), legend.title = element_blank())
     }
-    else {
+    else if (input$radio == 2){
       ggplot(pd, aes(EndDate, value, colour = factor(variable))) +
         stat_smooth(method = "lm", aes(fill = factor(variable))) +
         #geom_line() +
@@ -40,7 +40,30 @@ shinyServer(function(input, output) {
         ggtitle(paste0("HCAHPS Percent of Patients\nwho gave a 9 or 10 Rating")) +
         theme(axis.text.x = element_text(angle = 90, size = 14), axis.title.x = element_text(size = 14), axis.title.y = element_text(size = 12), legend.title = element_blank())
     }
-    
+    else if (input$radio == 3){
+      ggplot(pd, aes(EndDate, value, colour = factor(variable))) +
+        stat_smooth(method = "glm", aes(fill = factor(variable))) +
+        #geom_line() +
+        scale_x_date(breaks = filter(d, variable == input$var)$EndDate) +
+        scale_colour_discrete(breaks = levels(pd$variable)) +
+        geom_point(size = 5) +
+        xlab("12 months ending date") +
+        ylab("Percentage Rating of 9 or 10") +
+        ggtitle(paste0("HCAHPS Percent of Patients\nwho gave a 9 or 10 Rating")) +
+        theme(axis.text.x = element_text(angle = 90, size = 14), axis.title.x = element_text(size = 14), axis.title.y = element_text(size = 12), legend.title = element_blank())
+    }
+    else if (input$radio == 4){
+      ggplot(pd, aes(EndDate, value, colour = factor(variable))) +
+        stat_smooth(method = "loess", aes(fill = factor(variable))) +
+        #geom_line() +
+        scale_x_date(breaks = filter(d, variable == input$var)$EndDate) +
+        scale_colour_discrete(breaks = levels(pd$variable)) +
+        geom_point(size = 5) +
+        xlab("12 months ending date") +
+        ylab("Percentage Rating of 9 or 10") +
+        ggtitle(paste0("HCAHPS Percent of Patients\nwho gave a 9 or 10 Rating")) +
+        theme(axis.text.x = element_text(angle = 90, size = 14), axis.title.x = element_text(size = 14), axis.title.y = element_text(size = 12), legend.title = element_blank())
+    }
       
       #scale_x_discrete(breaks = unique(d.plot$EndDate))
   })
--- ui.R
+++ ui.R
@@ -11,12 +11,20 @@ shinyUI(fluidPage(
                   selected = "BETH ISRAEL DEACONESS MEDICAL CENTER"),
       br(),
       
-      checkboxInput("cbvar",
-                    label = "Add Smoothing",
-                    value = FALSE),
-      
+#       checkboxInput("cbvar",
+#                     label = "Add Smoothing",
+#                     value = FALSE),
+      br(),
+      radioButtons("radio", label = h5("Choose a Smoothing Method"),
+                  choices = list("No Smoothing" = 1, "Linear" = 2,
+                            "Generalized Linear" = 3, "Local (Loess)" = 4),selected = 1),
+
       br(),
       
+      p(a("ggplot2 smoothing resource", href = "http://docs.ggplot2.org/current/stat_smooth.html")),
+      
+      br(),      
+
       p("Data Source: ", a("CMS Hospital Compare", href = "https://data.medicare.gov/data/hospital-compare")),
       p("Code: ", a("Github Repository", href = "https://github.com/andylytics/hosp_compare")),
       p("Created by: ", a("Andy Rosa", href = "https://www.linkedin.com/pub/andrew-rosa/99/787/a64"))
